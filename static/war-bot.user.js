9a6f6e3e894ff81c951c0fdff49de01c04962d9b
Fries91 <46676958+Fries91@users.noreply.github.com>
2026-03-14 17:57:40 -0400

    Refactor card rendering logic in war-bot.user.js

--- static/war-bot.user.js
+++ static/war-bot.user.js
@@ -1365,12 +1365,25 @@ function renderOverviewTab() {
     }
 
     if (!cards.length) {
-        cards.push('\
-        <div class="warhub-card">\
-            <h3>Overview</h3>\
-            <div class="warhub-empty">No quick boxes selected. Turn them on in Settings.</div>\
-        </div>');
-    }
+    cards.push('\
+    <div class="warhub-card">\
+        <h3>Overview</h3>\
+        <div class="warhub-empty">No quick boxes selected. Turn them on in Settings.</div>\
+    </div>');
+}
+
+if (state && state.debug) {
+    cards.push('\
+    <div class="warhub-card">\
+        <div class="warhub-section-title">\
+            <h3>War Debug</h3>\
+            <span class="warhub-count">Live</span>\
+        </div>\
+        <div class="warhub-mini" style="white-space:pre-wrap; line-height:1.5;">' + esc(JSON.stringify(state.debug, null, 2)) + '</div>\
+    </div>');
+}
+
+return renderAccessBanner() + '<div class="warhub-grid two">' + cards.join('') + '</div>';
 
     return renderAccessBanner() + '<div class="warhub-grid two">' + cards.join('') + '</div>';
 }
